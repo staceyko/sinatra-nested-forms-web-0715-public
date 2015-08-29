fc872a2d1f2c395d0835a3f591d64fd97cb709b2
staceyko <ko.stacey@gmail.com>
2015-08-29 13:27:07 -0400

    done

--- app.rb
+++ app.rb
@@ -1,9 +1,23 @@
 require './environment'
-
+require 'pry'
 module FormsLab
   class App < Sinatra::Base
 
-    # code other routes/actions here
+    get '/' do
+      erb :root
+    end
+
+    get '/new' do
+      erb :"/pirates/new"
+    end
+
+    get '/show' do
+      erb :"/pirates/show"
+    end
+
+    post '/pirates' do
+      @pirate = params["pirate"]
+    end
 
   end
 end
